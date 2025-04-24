2f01506af05d96599d3b49471a5bd78465da627b
SupriyaJakkani <jakkanisupriya34@gmail.com>
2025-04-24 23:32:41 +0530

    commit

---
+++ project1.py
@@ -0,0 +1,15 @@
+#Madlib
+verb1 = input("Enter a verb1: ")
+adjective1 = input("Enter a adjective1: ")
+adjective2 = input("Enter a adjective2: ")
+verb2 = input("Enter a verb2: ")
+verb3 = input("Enter a verb3: ")
+adjective3 = input("Enter a adjective3: ")
+
+madlib =f"Every morning, she wakes up early and {verb1} to the {adjective1} park near her house.\
+The air is fresh, and the sky looks {adjective2}.\
+She {verb2} as she sees the happy children running and laughing. \
+Birds sing in the trees, and a friendly dog {verb3} around chasing a {adjective3} ball.\
+After a while, she sits on a soft bench to read her favorite book."
+
+print(madlib)
